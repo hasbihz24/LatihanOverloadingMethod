9264ee99b77f85c26d14c947121bb4006e3c08de
hasbihz24 <hasbihz24@gmail.com>
2022-04-08 15:42:51 +0700

    Tugas Method
    Nama : Hasbi Hawarizmi / 21.11.4411

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimun (int number1, int number2)
+        { 
+            if( number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        public int FindMinimun(int number1, int number2, float number3)
+        {
+            if(number1 < number2)
+            {
+                if(number1 < number3)
+                {
+                    return number1;
+                }
+                else
+                {
+                    return (int)number3;
+                }
+            }else if(number2 < number1)
+            {
+                if(number2 < number3)
+                {
+                    return number2;
+                }
+                else
+                {
+                    return (int)number3;
+                }
+            }
+            else
+            {
+                return (int)number3;
+            }
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            if (number1 > number2)
+            {
+                if (number1 > number3)
+                {
+                    return number1;
+                }
+                else
+                {
+                    return (int)number3;
+                }
+            }
+            else if (number2 > number1)
+            {
+                if (number2 > number3)
+                {
+                    return number2;
+                }
+                else
+                {
+                    return (int)number3;
+                }
+            }
+            else
+            {
+                return (int)number3;
+            }
+        }
+    }
+}
